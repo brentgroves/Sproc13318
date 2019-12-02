1d1d459ccf52338ba3674a854691efbd32a8e007
Brent Groves <brent.groves@gmail.com>
2019-12-02 15:27:24 -0500

    Added/Changed code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql');
 const mqtt = require('mqtt');
 const config = require('../Config13318/config.json');
+var datetime = require('node-datetime');
+var mqttClient;
 
 function DS13318(transDate) {
   var con = mysql.createConnection(config.Database);
@@ -9,17 +11,31 @@ function DS13318(transDate) {
     if (err) throw err;
 
     /* https://www.sitepoint.com/using-node-mysql-javascript-client/ */
-    con.query(`CALL DS13318('${transDate}')`,function(err, rows){
+    // con.query(`select * from DS13318`,function(err, resultSet){
+    // con.query(`CALL DS13318("2019-12-15 09:00")`,function(err, rows){
+    // con.query(`CALL DS13318('${transDate}')`,function(err, resultSet){
+    con.query('CALL DS13318(?)', [transDate], function(err, resultSet) {
       if (err) throw err;
-      console.log('Data received from Db:\n');
-      console.log(rows);
+      console.log(`Data received from Db on ${transDate}:\n`);
+      // console.log(resultSet[0][1]);
+      resultSet[0].forEach(function(item, index) {
+        var tz_transDate = item.TransDate;
+        var pastDateTime = datetime.create(tz_transDate);
+        var fmt = pastDateTime.format('Y-m-d H:M:S');
+        item.TransDate=fmt;  
+        // console.log(item, index);
+      });
+      console.log(resultSet[0]);
+      let msgString = JSON.stringify(resultSet[0]);         
+      mqttClient.publish('Sproc13318', msgString);         
     });
   });
 }
 
 function main() {
-  let mqttClient = mqtt.connect(config.MQTT);
+  mqttClient = mqtt.connect(config.MQTT);
 
+  DS13318('2019-12-15 09:00');
   mqttClient.on('connect', function() {
     mqttClient.subscribe('Alarm13318-2', function(err) {
       if (!err) {
